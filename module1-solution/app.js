b982183b0c25a20ad872d3c260bf749604abbea9
Stefan Duerig <stefan.duerig@rohde-schwarz.com>
2016-12-06 18:37:53 +0100

    Module1: Added: Bonus

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -5,14 +5,18 @@ angular.module('LunchCheckerApp', [])
 
 .controller('LunchCheckerController', function ($scope) {
   $scope.lunchItems = "";
-  $scope.stateOfLunchItems  = "Unknown";
+  $scope.lunchState = "---";
+  $scope.lunchStateStyle = "";
+  $scope.lunchTextBoxStyle = "";
 
   $scope.calculateLunchItems = function () {
-    var countOfLunchItems = calculatNumericForString($scope.lunchItems);
-    $scope.stateOfLunchItems = calculatState(countOfLunchItems);
+    var countOfLunchItems = calculateNumericForString($scope.lunchItems);
+    $scope.lunchState = calculateState(countOfLunchItems);
+    $scope.lunchStateStyle = calculateStyle(countOfLunchItems);
+    $scope.lunchTextBoxStyle = calculateTextBoxStyle(countOfLunchItems);
   };
 
-  function calculatNumericForString(string) {
+  function calculateNumericForString(string) {
     var totalStringValue = 0;
 
     if (string.length > 0) {
@@ -28,20 +32,33 @@ angular.module('LunchCheckerApp', [])
     return totalStringValue;
   }
 
-  function calculatState(count) {
+  function calculateState(count) {
     if (count == 0) {
-        return "Please enter data first";
+      return "Please enter data first";
     }
 
     if (count > 3) {
       return "Too Much!";
     }
 
-
-
     return "Enjoy!";
   }
 
+  function calculateStyle(count) {
+    if (count <= 0) {
+      return { "color" : "red", "background-color" : "white"};
+    }
+
+    return { "color" : "green", "background-color" : "white" };
+  }
+
+  function calculateTextBoxStyle(count) {
+    if (count <= 0) {
+      return { "color" : "black", "background-color" : "white", "border-style" : "solid", "border-color" : "red" };
+    }
+
+    return { "color" : "black", "background-color" : "white", "border-style" : "solid", "border-color" : "green" };
+  }
 });
 
 
